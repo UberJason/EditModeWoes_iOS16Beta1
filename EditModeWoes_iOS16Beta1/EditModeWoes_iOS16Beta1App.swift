d55ce9cf3a3aec3352b352946ace86ca0195fcb5
Jason Ji <uberjason@gmail.com>
2022-06-11 13:54:23 -0400

    Initial Commit

---
+++ EditModeWoes_iOS16Beta1/EditModeWoes_iOS16Beta1App.swift
@@ -0,0 +1,17 @@
+//
+//  EditModeWoes_iOS16Beta1App.swift
+//  EditModeWoes_iOS16Beta1
+//
+//  Created by Jason Ji on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct EditModeWoes_iOS16Beta1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
